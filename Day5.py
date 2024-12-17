0fa8d8f776bb5d3b510c9b44c5cd2e007e4754df
unknown <sharmeela048@gmail.com>
2024-12-17 16:36:58 +0545

    any message

---
+++ Day5.py
@@ -0,0 +1,5 @@
+### commands
+
+git init . # git initialize, start folder to track, . is current folder
+
+git status # check git status 
---
+++ string_1.py
@@ -0,0 +1,15 @@
+first_number = float(input("Enter your first number :"))
+second_number = float(input("Enter your second number :"))
+
+user_choice = input("""
+                  Please choose your option 
+                     + for Addition 
+                     - for Substraction 
+
+                     """)
+if user_choice == "+":
+      print ("Addition = ", first_number + second_number)
+elif user_choice == "-": 
+      print (substratcion = ", first_number - second number)
+else: 
+      print ("Invalid option")
