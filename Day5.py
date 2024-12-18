a78d43eecff28fafd1ba6dd13d227dabfdb7f2f5
sarmila <sharmeela048@gmail.com>
2024-12-18 16:47:29 +0545

    message

--- Day5.py
+++ Day5.py
@@ -1,5 +1,15 @@
 ### commands
 
+from webbrowser import get
+
+
 git init . # git initialize, start folder to track, . is current folder
 
 git status # check git status 
+git add . # staging area, . is all files
+
+git config --global user.email "sharmeela048@gmail.com"
+git config --global user.name "sarmila"
+git remote add origin https://github.com/Sarmila-code/python_class.git
+
+git push origin master # code push to git
